fe345dc6ba43422952029a3de7212d65ed689668
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2022-10-28 21:31:10 +0800

    Created a java file

---
+++ main.java
@@ -0,0 +1,43 @@
+   import java.util.Scanner;
+
+   
+
+   public class Main {
+
+	static Scanner castillo_scan = new Scanner(System.in);	      private static int Percent, Number, TotalNum, Case, Case2;
+
+	
+
+	public static void Input(){
+
+	
+
+		System.out.print("INPUT OBTAINED  VALUE: ");
+
+		Number = castillo_scan.nextInt();
+
+		System.out.print("INPUT THE TOTAL VALUE VALUE: ");
+
+		TotalNum = castillo_scan.nextInt();
+
+	}
+
+	public static void Output(){
+
+        Case = Number*100/TotalNum;
+
+	    System.out.println(" ");   
+
+		System.out.println(Case+"%");                                    
+
+	}
+
+	public static void main(String[] args) {
+
+		Input();
+
+		Output();
+
+	}
+
+}
